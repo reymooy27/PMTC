c8deca7fce07d649a4566d03ac933bf54cb43832
Rey Mooy <gdrrey@gmail.com>
2020-08-03 13:39:15 +0800

    fix deleteUnconfirmed

--- utils/deleteUnconfirmedParticipant.js
+++ utils/deleteUnconfirmedParticipant.js
@@ -5,12 +5,9 @@ module.exports = async (req, res, next) => {
   const participant = await Participant.find({ confirmed: false });
   participant.forEach((p) => {
     if (Date.now() - p.registerDate > 43200000) {
-      cloudinary.v2.uploader.destroy(
-        `logo/${participant.teamName}`,
-        (error, result) => {
-          console.log(result, error);
-        }
-      );
+      cloudinary.v2.uploader.destroy(`logo/${p.teamName}`, (error, result) => {
+        console.log(result, error);
+      });
       p.deleteOne((err, prod) => {
         if (err) console.log(err);
         console.log(`${prod.teamName} dihapus`);
