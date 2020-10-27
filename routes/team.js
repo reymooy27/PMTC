c38172fcb060bf65300c1c1ba6773e16fc1d479c
Rey Mooy <gdrrey@gmail.com>
2020-10-27 13:19:21 +0800

    add verify to route

--- routes/team.js
+++ routes/team.js
@@ -1,11 +1,12 @@
 const router = require("express").Router();
 const { upload } = require("../utils/uploadLogo");
 const teamCtrl = require("../controllers/team.controller");
+const verify = require('../utils/verifyToken');
 
 router.post("/api/v1/teams", teamCtrl.getAllTeam);
 router.post("/team/create/:id", upload, teamCtrl.createTeam);
 router.post("/team/:id",teamCtrl.getTeamByID);
-router.put("/team/update/:id", teamCtrl.updateTeam);
-router.delete("/team/delete/:id", teamCtrl.deleteTeam);
+router.put("/team/update/:id", verify, teamCtrl.updateTeam);
+router.delete("/team/delete/:id", verify, teamCtrl.deleteTeam);
 
 module.exports = router;
--- routes/tournament.js
+++ routes/tournament.js
@@ -1,8 +1,9 @@
 const router = require("express").Router();
 const tournamentCtrl = require("../controllers/tournament.controller");
+const verify = require('../utils/verifyToken');
 
-router.post("/tournament/create", tournamentCtrl.createTournament);
-router.put("/tournament/:id/update", tournamentCtrl.updateTournament);
+router.post("/tournament/create", verify, tournamentCtrl.createTournament);
+router.put("/tournament/:id/update", verify, tournamentCtrl.updateTournament);
 router.post("/api/v1/tournaments", tournamentCtrl.getAllTournament);
 router.post('/tournament/:id', tournamentCtrl.getTournamentByID)
 
