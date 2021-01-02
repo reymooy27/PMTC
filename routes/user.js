d15bb02bf515e9bf72065d52143c7e49c91ea1d2
Rey Mooy <gdrrey@gmail.com>
2021-01-02 14:59:00 +0800

    add some new routes

--- routes/user.js
+++ routes/user.js
@@ -3,13 +3,17 @@ const userCtrl = require('../controllers/user.controller')
 const verify = require('../utils/verifyToken');
 const {uploadProfilePicture} = require('../utils/uploadProfilePicture')
 
+router.post("/user/all", userCtrl.getAllUser);
 router.post("/user/:id", userCtrl.getUserByID);
-router.post("/user/:id/team/create", verify, userCtrl.createTeam);
+router.patch("/user/:id/update", verify, userCtrl.updateUserProfile);
 router.patch("/user/:id/profilePicture/upload",verify, uploadProfilePicture, userCtrl.updateProfilePicture);
-router.post("/tournament/:idTournament/team/:teamId/join",verify, userCtrl.joinTournament);
+router.post("/user/:id/game/pubgMobile/add", verify, userCtrl.addAccountPUBGMobile);
+router.patch("/user/:id/game/pubgMobile/update", verify, userCtrl.updateUserPubgMobileStats);
+router.post("/user/:id/team/create", verify, userCtrl.createTeam);
+router.delete("/user/team/:teamId/delete",verify, userCtrl.deleteTeam);
 router.post("/team/:teamID/user/:userID/add",verify, userCtrl.addPlayerToTeam);
 router.post("/team/:teamID/user/:userID/remove",verify, userCtrl.removePlayerFromTeam);
-router.delete("/user/team/:teamId/delete",verify, userCtrl.deleteTeam);
+router.post("/tournament/:idTournament/team/:teamId/join",verify, userCtrl.joinTournament);
 router.post("/signup",userCtrl.signUp );
 router.post("/login", userCtrl.login);
 router.get("/logout", userCtrl.logout);
