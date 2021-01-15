91bcc1192d7a67b659528e52882fe4820fad470a
Rey Mooy <gdrrey@gmail.com>
2021-01-16 02:08:41 +0800

    add soket io

--- server.js
+++ server.js
@@ -4,26 +4,42 @@ const mongoose = require("mongoose");
 const compression = require("compression");
 const helmet = require("helmet");
 const cors = require("cors");
+const http = require('http');
+const socketio = require('socket.io')
 const cookieParser = require('cookie-parser')
 const route = require("./routes/routes");
 const tournamentRoutes = require("./routes/tournament");
 const userRoutes = require("./routes/user");
 const teamRoutes = require("./routes/team");
-const gameRoutes = require('./routes/game')
+const gameRoutes = require('./routes/game');
+const chatRoutes = require('./routes/chat')
 const deleteUnconfirmedTeam = require("./utils/deleteUnconfirmedTeam");
 const app = express();
 const port = process.env.PORT || 8000;
 const db_uri = process.env.DB_CONNECT;
+
+const server = http.createServer(app);
+const io = socketio(server,{
+  cors: {
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST"],
+    credentials: true
+  }
+});
 
 dotenv.config();
 app.use(compression());
 app.use(cookieParser())
 app.use(helmet());
 // Production
-app.use(cors({credentials: true, origin: process.env.FRONTEND_URL}));
+// app.use(cors({credentials: true, origin: process.env.FRONTEND_URL}));
 // Development
-// app.use(cors({credentials: true, origin: 'http://localhost:3000'}));
+app.use(cors({credentials: true, origin: 'http://localhost:3000'}));
 // app.use(deleteUnconfirmedTeam);
+app.use(function (req, res, next) {
+  req.io = io;
+  next();
+});
 
 mongoose.connect(
   db_uri,
@@ -57,5 +73,15 @@ app.use(tournamentRoutes);
 app.use(userRoutes);
 app.use(teamRoutes);
 app.use(gameRoutes);
+app.use(chatRoutes);
 
-app.listen(port, ()=> console.log('server on'));
+io.on("connection", (socket) => {
+  socket.on('click',(arg)=>{
+    console.log(arg);
+  })
+  socket.on('disconnecting',(reason)=>{
+    console.log('disconnecting');
+  })
+});
+
+server.listen(port, ()=> console.log('server on'));
