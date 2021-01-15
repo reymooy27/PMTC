34fb1d8603efb38feeffd9e364d7472ce59fdde1
Rey Mooy <gdrrey@gmail.com>
2021-01-16 02:05:48 +0800

    chat routes

---
+++ routes/chat.js
@@ -0,0 +1,8 @@
+const router = require('express').Router();
+const chatCtrl = require('../controllers/chat.controllers')
+const verify = require('../utils/verifyToken')
+router.post('/chat/:id', verify, chatCtrl.sendMessage)
+router.post('/chats/:id', verify, chatCtrl.getUserConversation)
+router.post('/chatList', verify, chatCtrl.getUserChatList)
+
+module.exports = router
