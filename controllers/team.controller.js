5940cb6fc7fb6b9ce7305a3c9b97ef75e8cfc4f0
Rey Mooy <gdrrey@gmail.com>
2021-02-06 02:28:01 +0800

    add team invite, remove player

--- controllers/team.controller.js
+++ controllers/team.controller.js
@@ -5,6 +5,9 @@ const sendEmail = require("../utils/sendEmail");
 const Tournament = require("../model/tournament");
 const Team2 = require("../model/team2");
 const User = require("../model/user");
+const FR = require('../model/friendRequest')
+const Notification = require('../model/notification')
+
 
 const createTeam = async (req, res) => {
   const {teamName,
@@ -280,7 +283,10 @@ const createUserTeam = async (req, res)=>{
   const teamExist = await Team2.findOne({teamName: req.body.teamName})
   if(teamExist) return res.status(400).json('Nama tim sudah terpakai')
 
-  const team = new Team2(req.body);
+  const team = new Team2({
+    teamName: req.body.teamName,
+    createdBy: req.user._id
+  });
 
   try {
     const user = await User.findByIdAndUpdate({_id: req.params.id},{$push: {myTeam: team},},{new: true, upsert:true })
@@ -316,30 +322,109 @@ const deleteUserTeam = async (req,res)=>{
   }
 }
 
-const addPlayerToTeam = async (req,res)=>{
-  try {
-    const team = await Team2.findById({_id: req.params.teamID})
-    if(team.roster.includes(req.params.userID)) return res.status(400).json('Pemain sudah terdaftar dalam tim')
-
-    await Team2.updateOne({_id: req.params.teamID}, {'$push': {'roster': req.params.userID}})
-    await User.updateOne({_id: req.params.userID}, {'$push': {'myTeam': req.params.teamID}})
-    res.status(200).json('Berhasil menambahkan pemain ke tim')
-  } catch (error) {
-    res.status(400).json('Gagal menambahkan pemain ke tim')
-  }
-}
 
 const removePlayerFromTeam = async (req,res)=>{
   try {
     await Team2.updateOne({'roster': req.params.userID}, {'$pull': {'roster': req.params.userID}})
     await User.updateOne({'myTeam': req.params.teamID}, {'$pull': {'myTeam': req.params.teamID}})
-    res.status(200).json('Berhasil menghapus pemain dari tim')
+    req.io.sockets.emit('removePlayer', 'Berhasil mengeluarkan pemain dari tim')
+    res.status(200).json('Berhasil mengeluarkan pemain dari tim')
   } catch (error) {
-    res.status(400).json('Gagal menghapus pemain dari tim')
+    res.status(400).json('Gagal mengeluarkan pemain dari tim')
   }
 }
 
+const teamInvite = async (req,res)=>{
+  try {
+    const request = await FR.findOne({
+      $or: [
+          { $and: [{ to: req.user._id }, { from: req.params.id },{requestType: 'TEAM_INVITATION'}] },
+          { $and: [{ to: req.params.id }, { from: req.user._id },{requestType: 'TEAM_INVITATION'}] },
+      ],
+        })
+    if(request) return res.status(400).json('Undangan anda sudah terkirim')
 
+    const team = await Team2.findOne({'roster': req.params.id})
+    if(team) return res.status(400).json('Pemain sudah berada dalam tim')
+
+    const newRequest = new FR({
+      from: req.user._id,
+      to: req.params.id,
+      requestType: 'TEAM_INVITATION',
+      teamId: req.body.teamId
+    })
+    newRequest.save()
+    req.io.sockets.emit('sendTeamInvitation', newRequest._id)
+    const newNotification = new Notification({
+      sender: req.user._id,
+      message: 'Anda memiliki undangan tim baru',
+      recievers: [req.params.id],
+      link: `/profile/team/${req.body.teamId}`,
+      action: [
+        {
+          actionType: 'Terima',
+          actionLink: `/team/invite/update/${newRequest._id}`
+        },
+        {
+          actionType: 'Tolak',
+          actionLink: `/team/invite/update/${newRequest._id}`
+        },
+    ]
+    })
+    newNotification.save()
+    req.io.sockets.emit('notification', newNotification._id)
+    res.status(200).json('Berhasil mengirim undangan tim')
+  } catch (error) {
+    res.status(400).json('Gagal mengirim undangan tim')
+  }
+}
+
+const updateTeamInvite = async (req,res)=>{
+  try {
+    const request = await FR.findById({_id: req.params.id})
+    const notif = await Notification.findOne({$and: [{sender: request.from}, {recievers: [req.user._id]},{message: 'Anda memiliki undangan tim baru'}]})
+    if(req.body.accepted === true){
+      const team = await Team2.findById({_id: request.teamId })
+      team.roster.push(request.to)
+      team.save()
+      const user = await User.findById({_id: request.to})
+      user.myTeam.push(team)
+      user.save()
+      req.io.sockets.emit('sendTeamInvitation', request._id)
+      const newNotification = new Notification({
+        sender: request.to,
+        message: 'Undangan tim anda diterima',
+        recievers: [request.from],
+        link: `/profile/team/${request.teamId}`
+      })
+      newNotification.save()
+      notif.remove()
+      request.remove()
+      req.io.sockets.emit('notification', newNotification._id)
+      return res.status(200).json('Permintaan pertemanan anda diterima')
+    }else{
+      notif.remove()
+      request.remove()
+      req.io.sockets.emit('notification', 'Permintaan pertemanan anda ditolak')
+      return res.status(200).json('Permintaan pertemanan anda ditolak')
+    }
+  } catch (error) {
+    res.status(400).json('Gagal mengudate permintaan pertemanan')
+  }
+}
+
+const cancelTeamInvite = async (req,res)=>{
+  try {
+    const requests = await FR.findOne({$and:[{from: req.user._id},{to: req.params.id},{requestType: 'TEAM_INVITATION'}]})
+    const notif = await Notification.findOne({$and: [{sender: req.user._id}, {recievers: [req.params.id]},{message: 'Anda memiliki undangan tim baru'}]})
+    notif.remove()
+    requests.remove()
+    req.io.sockets.emit('notification', 'Berhasil membatalkan undangan tim')
+    res.status(200).json('Berhasil membatalkan undangan tim')
+  } catch (error) {
+    res.status(400).json('Gagal membatalkan undangan tim')
+  }
+}
 
 module.exports = {
   createTeam,
@@ -350,6 +435,9 @@ module.exports = {
   getTeam2ByID,
   createUserTeam,
   deleteUserTeam,
-  addPlayerToTeam,
+  teamInvite,
+  updateTeamInvite,
+  cancelTeamInvite,
   removePlayerFromTeam,
+  
 };
